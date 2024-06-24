adb9c8089e786fcf4390241e67295ad808b5a85f
punam10511 <punam10511@gmail.com>
2024-06-23 21:46:06 -0400

    revised

--- part1.py
+++ part1.py
@@ -1,7 +1,9 @@
 import sys
+from datetime import datetime
 
 for line in sys.stdin:
     data = line.strip().split("\t")
     if len(data) == 6:
+        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         date, time, store, item, cost, payment = data
         print("{0}\t{1}".format(item, cost))
